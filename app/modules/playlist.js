55572e50e1ed1f35da20ea83cbda7ae42f0d17a0
Eric Demicco <edemicco@gmail.com>
2012-10-11 19:50:52 -0400

    Styling for playlist, etc.

--- app/modules/playlist.js
+++ app/modules/playlist.js
@@ -14,16 +14,23 @@ function(app, Backbone)
 
 	App.Views.PlaylistView = Backbone.LayoutView.extend({
 		initialize: function() {
-			_.bindAll(this, 'render', 'togglePlaylist');
+			_.bindAll(this, 'render', 'togglePlaylist', 'goToTime');
 		},
 		template : 'playlist',
 		events : {
-			'click .toggle-playlist' : 'togglePlaylist'
+			'click .toggle-playlist' : 'togglePlaylist',
+			'click .progress-bar' : 'goToTime'
 		},
 		togglePlaylist: function(e) {
 			$(e.target).toggleClass('open');
 
 			$('.playlist-container').stop().slideToggle();
+		},
+		goToTime: function(e) {
+			// for now this just updates the progress bar
+			var progressBar = $(e.currentTarget);
+			var percentClicked = ( (e.pageX - progressBar.offset().left) / progressBar.width() ) * 100;
+			this.$('.elapsed').css('width',percentClicked + '%');
 		}
 	});
 
