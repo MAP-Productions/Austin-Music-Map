051879ca59d9fc6f389cb845d1ce3206ff309351
Joseph Bergen <jbergen@gmail.com>
2012-11-06 18:48:32 -0500

    playlist dropdown hooked up

--- app/modules/submodules/all-playlists.js
+++ app/modules/submodules/all-playlists.js
@@ -12,10 +12,29 @@ function(App, Backbone )
 	AllPlaylists.View = Backbone.LayoutView.extend({
 
 		template : 'all-playlists',
+
+		initialize : function()
+		{
+			App.playlistCollection.on('reset', this.onSync, this);
+		},
+
+		onSync : function()
+		{
+			console.log('all playlists // sync', App.playlistCollection, App.playlistCollection.length, App.playlistCollection.toJSON() );
+			this.render();
+		},
+
+		serialize : function()
+		{
+			console.log('serialize ', App.playlistCollection.toJSON());
+			return {playlists:App.playlistCollection.toJSON()};
+		},
+
 		events : {
 			'click .toggle-playlists' : 'togglePlaylists'
 		},
-		togglePlaylists : function() {
+		togglePlaylists : function()
+		{
 			this.$('.all-playlists-container').slideToggle();
 		}
 
