49324091aabc2c32c77d4c5efdf8a629ceaab3a9
Joseph Bergen <jbergen@gmail.com>
2012-11-13 18:52:03 -0500

    udpated slideshow layer with thumbnail slider

--- app/modules/submodules/soundscape.js
+++ app/modules/submodules/soundscape.js
@@ -13,29 +13,29 @@ function(App, Backbone)
 
 	Soundscape.initialize=function(){
 	
-		var _this=this;
-		this.loaded=false;
-		var audio = $('<audio>').attr({'id':'amm-soundscape'});
-		audio.on('canplay',function(){console.log('soundscape can play');});
-		audio.on('canplaythrough',function(){
-			if(App.page&&App.page.type=='Map') {
-				this.play();
-			}
-			_this.loaded=true;
-		});
-		audio.attr({'src':'assets/audio/soundscape.mp3'}).appendTo('body');
+		// var _this=this;
+		// this.loaded=false;
+		// var audio = $('<audio>').attr({'id':'amm-soundscape'});
+		// audio.on('canplay',function(){console.log('soundscape can play');});
+		// audio.on('canplaythrough',function(){
+		// 	if(App.page&&App.page.type=='Map') {
+		// 		this.play();
+		// 	}
+		// 	_this.loaded=true;
+		// });
+		// audio.attr({'src':'assets/audio/soundscape.mp3'}).appendTo('body');
 
 
 
-		for(var i=1;i<=4;i++){
+		// for(var i=1;i<=4;i++){
 
-			var j=4+i;
-			$('body').append($('<audio>').attr({'src':'assets/audio/static'+i+'.mp3','id':'amm-static-'+i}));
-			$('body').append($('<audio>').attr({'src':'assets/audio/ding'+i+'.mp3','id':'amm-ding-'+i}));
-			$('body').append($('<audio>').attr({'src':'assets/audio/ding'+i+'.mp3','id':'amm-ding-'+j}));
+		// 	var j=4+i;
+		// 	$('body').append($('<audio>').attr({'src':'assets/audio/static'+i+'.mp3','id':'amm-static-'+i}));
+		// 	$('body').append($('<audio>').attr({'src':'assets/audio/ding'+i+'.mp3','id':'amm-ding-'+i}));
+		// 	$('body').append($('<audio>').attr({'src':'assets/audio/ding'+i+'.mp3','id':'amm-ding-'+j}));
 
 
-		}
+		// }
 
 
 	};
@@ -51,11 +51,11 @@ function(App, Backbone)
 	};
 
 	Soundscape.ding = function(){
-		var dingNo=Math.floor(1+Math.random()*8);
-		var ding=document.getElementById('amm-ding-'+dingNo);
-		ding.currentTime=0;
-		ding.play();
-		console.log(dingNo);
+		// var dingNo=Math.floor(1+Math.random()*8);
+		// var ding=document.getElementById('amm-ding-'+dingNo);
+		// ding.currentTime=0;
+		// ding.play();
+		// console.log(dingNo);
 	};
 
 
