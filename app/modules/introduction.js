0ef59600796404647a7a693328cf2213f88bc4d0
Eric Demicco <edemicco@gmail.com>
2012-11-16 17:26:53 -0500

    Add introduction screen. Plays on first visit determined by cookie.

--- app/modules/introduction.js
+++ app/modules/introduction.js
@@ -11,23 +11,35 @@ function(App, Backbone) {
 
 	Introduction.View = Backbone.LayoutView.extend({
 		template : 'introduction',
+		events: {
+			'click .next': 'exitIntroduction'
+		},
 		initialize : function() {
 			_.bindAll(this, 'afterRender');
 		},
 		afterRender : function() {
-			var _this = this;
+			var animLength = 10000,
+				_this = this;
+
 			this.els = {
-				overlay: this.$('.intro-overlay')
+				overlay: this.$('.intro-overlay'),
+				toFade: this.$('.fade')
 			};
+
+			this.els.toFade.each(function(i,v) {
+				$(this).delay( ( ( animLength / (_this.els.toFade.length + 1 ) ) * i ) + 500 ).fadeIn(500);
+			});
+
 			setTimeout(function() {
 				_this.exitIntroduction();
-			}, 10000);
+			}, animLength + 3000);
 		},
 
 		exitIntroduction : function()
 		{
+			var _this = this;
 			this.els.overlay.fadeOut(1000, function() {
-				this.remove();
+				_this.remove();
 			});
 		}
 	});
