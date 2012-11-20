8dc75195d9661111a3df7817f1f1560c8ccdfb49
Eric Demicco <edemicco@gmail.com>
2012-11-20 14:59:54 -0500

    Files for instruction circle.

---
+++ app/modules/mini-intro.js
@@ -0,0 +1,37 @@
+define([
+	"app",
+	// Libs
+	"backbone"
+],
+
+function(App, Backbone) {
+
+	// Create a new module
+	var MiniIntro = Zeega.module();
+
+	MiniIntro.View = Backbone.LayoutView.extend({
+		template : 'mini-intro',
+		initialize: function() {
+			var _this = this;
+			_.bindAll(this, 'removeMiniIntro');
+			$('body').on('click', function() {
+				_this.remove();
+				$('body').off('click');
+			});
+		},
+		afterRender: function() {
+			setTimeout(this.removeMiniIntro, 6500);
+		},
+		removeMiniIntro : function()
+		{
+			var _this = this;
+			this.$('.mini-intro').fadeOut(1000, function() {
+				_this.remove();
+			});
+		}
+	});
+
+
+	// Required, return the module for AMD compliance
+	return MiniIntro;
+});
