08c73121ade8d0913361457f8194952ce363e5d3
Joseph Bergen <jbergen@gmail.com>
2012-12-04 14:29:28 -0500

    updated zeegaplayer

--- app/modules/submodules/player-loader.js
+++ app/modules/submodules/player-loader.js
@@ -32,7 +32,6 @@ initialize : function()
 			player.on('layer_loading', this.addLoadingItem, this);
 			player.on('layer_ready', this.onLayerLoaded, this);
 			player.on('visual_ready', this.onPlayerReady, this);
-
 		},
 
 		updateLoaderTitle : function( player )
@@ -41,27 +40,23 @@ initialize : function()
 			if( this.model.get('playlist_title') ){
 				if(this.model.get('playlist_title')=='Recently Added') $('.player-loader-header').find('h2').html("Loading media recently added to the Austin Music Map!");
 				else $('.player-loader-header').find('h2').html("Loading all media associated with: <span class='loader-title'>"+_this.model.get('playlist_title')+"</span></h2>");
-				
 			}
 			else
 			{
 				this.model.on('change:playlist_title', function(){
-
-
-
-
 					if(_this.model.get('playlist_title')=='Recently Added') $('.player-loader-header').find('h2').html("Loading media recently added to the Austin Music Map");
 					else $('.player-loader-header').find('h2').html("Loading all media associated with: <span class='loader-title'>"+_this.model.get('playlist_title')+"</span></h2>");
 				
-					
-
 				});
 			}
+
 		},
 
 		addLoadingItem : function( layerdata )
 		{
 			if(layerdata.type != 'SlideShow') this.$('.layer-loading').append('<li class="unloaded" data-id="'+ layerdata.id +'"><div class="col-left"><i class="zicon-'+ layerdata.attr.media_type.toLowerCase() +' loader-media-icon"></i></div>'+ layerdata.attr.title + ' by '+ htmlDecode(layerdata.attr.media_creator_username) +'</li>');
+			console.log('------player', this.player.getProjectData() );
+
 		},
 
 		onLayerLoaded : function( layerdata )
